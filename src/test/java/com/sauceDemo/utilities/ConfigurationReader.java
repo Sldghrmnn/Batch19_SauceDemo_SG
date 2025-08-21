fef0968da3529964839dae820622cc0b1d68f6f6
SeldaGehrmann <seldagehrmann75@gmail.com>
2025-08-21 20:39:11 +0200

    first commit

---
+++ src/test/java/com/sauceDemo/utilities/ConfigurationReader.java
@@ -0,0 +1,34 @@
+package com.sauceDemo.utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties;
+
+    static {
+        try {
+
+            String path = "configuration.properties";
+
+            /**
+             * FileInputStream -> Javaya dışarıdan veri almak için kullanılır
+             * FileOutputStream - > Javadan dışaruya veri vermek için kullanılır.
+             */
+            FileInputStream input = new FileInputStream(path);
+
+            properties = new Properties();
+
+            properties.load(input);
+
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static String get(String keyName){
+        return properties.getProperty(keyName);
+    }
+
+}
